5b728cb1c3f439067abf0770225f9fc7ad29773d
Viktor Pishuk <vpishuk@Viktors-MacBook-Pro.local>
2019-08-08 06:50:07 +0300

    finalizes mwis

--- mwis/mwis.js
+++ mwis/mwis.js
@@ -7,29 +7,31 @@ function maxWeighteIndependentdSet(list) {
         res[i] = Math.max(res[i - 1], res[i - 2] + list[i - 1])
         console.log('i-1: ', res[i - 1], 'i-2:', res[i - 2], 'vi:', list[i - 1], 'res', res[i])
     }
+    console.log('resulted line', ...res)
     let set = []
-    let i = res.length
+    let i = res.length - 1
     while(i >= 1) {
-        if (res[i - 1] >= res[i - 2] + list[i]) {
+        if (res[i] === res[i - 1]) {
             i--
         } else {
-            set.push(list[i])
+            set.push(list[i - 1])
             i-=2
         }
     }
+    console.log('set of vertices', ...set.reverse())
     return set
 }
 
 function test(file) {
     readFileAsArray(path.resolve(__dirname, file))
     .then(list => {
+        const vertices = [1, 2, 3, 4, 17, 117, 517, 997].map(d => list[d - 1])
+        console.log('look for vertices', ...vertices)
         const set = maxWeighteIndependentdSet(list);
-        const vertices = [1, 2, 3, 4, 17, 117, 517, 997].map(d => list[d])
-        console.log(...Array.from(set).reverse())
-        console.log(...vertices)
-        const answer = vertices.map(d => set.includes(d) ? 1 : 0).join('')
+
+        const answer = vertices.map(d => d && set.includes(d) ? 1 : 0).join('')
         console.log(answer);
     });
 }
 
-test('data/test.txt'); // 10010000 ?
+test('data/mwis.txt'); // 10100110
